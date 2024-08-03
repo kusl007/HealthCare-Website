0bb7eec69e6cd206db638ab8ea148833f7945b08
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-03 13:00:36 +0545

    files changed

--- app/page.tsx
+++ app/page.tsx
@@ -13,19 +13,22 @@ const Home = ({ searchParams }: SearchParamProps) => {
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
+          <div className="logo flex items-start h-[10vh] gap-3 mb-5">
           <Image
-            src="/assets/icons/logo-full.svg"
+            src="/assets/icons/logo-icon.svg"
             height={1000}
             width={1000}
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
+         <h1 className="text-4xl font-bold"> Our HealthCare</h1>
+          </div>
 
           <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CarePluse
+              © 2024 Our HealthCare
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
